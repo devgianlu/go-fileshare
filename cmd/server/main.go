5b5913968c0da4e6c611558888114bd6af77515d
devgianlu <altomanigianluca@gmail.com>
2023-12-23 23:33:39 +0100

    feat: load config from file or env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,38 @@ import (
 	"github.com/devgianlu/go-fileshare/auth"
 	"github.com/devgianlu/go-fileshare/http"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
+
+type Config struct {
+	Port     int
+	Secret   string
+	LogLevel string
+}
+
+func loadConfig() (*Config, error) {
+	viper.SetDefault("logLevel", "info")
+
+	// load config from local "server.yml" file
+	viper.AddConfigPath(".")
+	viper.SetConfigName("server")
+	viper.SetConfigType("yml")
+
+	// try to load from env
+	viper.AutomaticEnv()
+
+	// load from file
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
 
 type Server struct {
 	Auth fileshare.AuthProvider
@@ -13,14 +44,21 @@ type Server struct {
 }
 
 func main() {
-	const PORT = 8080         // FIXME
-	const SECRET = "test1234" // FIXME
+	cfg, err := loadConfig()
+	if err != nil {
+		log.WithError(err).Fatal("cannot load config")
+	}
 
-	log.SetLevel(log.TraceLevel)
+	logLevel, err := log.ParseLevel(cfg.LogLevel)
+	if err != nil {
+		log.WithError(err).Fatalf("invalid log level")
+	}
+
+	log.SetLevel(logLevel)
 
 	s := Server{}
-	s.Auth = auth.NewJWTAuthProvider([]byte(SECRET))
-	s.HTTP = http.NewHTTPServer(PORT, s.Auth)
+	s.Auth = auth.NewJWTAuthProvider([]byte(cfg.Secret))
+	s.HTTP = http.NewHTTPServer(cfg.Port, s.Auth)
 
 	if err := s.HTTP.ListenForever(); err != nil {
 		log.WithError(err).Fatalf("failed listening")
